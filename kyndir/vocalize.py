df6a53d3aea7c3b6c1de209b495e6145a16a8ecf
j@mhy <j@mhy.com>
2022-11-09 00:02:06 +0800

    vocalize main funcitonality finished

--- kyndir/vocalize.py
+++ kyndir/vocalize.py
@@ -2,39 +2,45 @@
 
 import torchaudio
 
-#print out sys.path to see the paths python uses to search for modules when importing.
-import sys
-print(sys.path)
+import os
+
+# #print out sys.path to see the paths python uses to search for modules when importing.
+# import sys
+# print(sys.path)
 
 
 from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_audio, load_voice, load_voices
 
 
-# This will download all the models used by Tortoise from the HuggingFace hub.
-tts = TextToSpeech()
-
+def init_tts():
+    # This will download all the models used by Tortoise from the HuggingFace hub.
+    tts = TextToSpeech()
+    return tts
 
 # Load it and send it through Tortoise.
-def vocalize_txt(txt_2b_voiced, voice_name, preset_mode, o_audio_flnm):
+def vocalize_txt(tts, txt_2b_voiced, voice_name, preset_mode, o_audio_flnm, extra_voice_dirs=[]):
     """_summary_
 
     Args:
-        txt_2b_voiced (_type_): _description_
+        tts: tts instance
+        txt_2b_voiced (_type_): This is the text that will be spoken.
         voice_name (_type_): choose a voice name, i.e. a subfolder name in the tortoise/voices
         preset_mode (_type_): Pick a "preset mode" to determine quality. Options: {"ultra_fast", "fast" (default), "standard", "high_quality"}. See docs in api.py
         o_audio_flnm (_type_): the output audio's name with extension
     """
-    voice_samples, conditioning_latents = load_voice(voice_name)
+    voice_samples, conditioning_latents = load_voice(voice_name, extra_voice_dirs)
     gen = tts.tts_with_preset(txt_2b_voiced, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
                               preset=preset_mode)
     torchaudio.save(o_audio_flnm, gen.squeeze(0).cpu(), 24000)
 
 
-# This is the text that will be spoken.
+# Main Action ----------------------------------------------------------------------------------------------------------
+
 txt_2b_voiced = "Joining two modalities results in a surprising increase in generalization! What would happen if we combined them all?"
-# Pick a "preset mode" to determine quality. Options: {"ultra_fast", "fast" (default), "standard", "high_quality"}. See docs in api.py
-preset_mode = "fast"
-# choose a voice name, i.e. a subfolder name in the tortoise/voices
-voice_name = "tom"
-vocalize_txt(txt_2b_voiced, voice_name, preset_mode, "venti_test.wav")
+# get my custome voices dir
+MY_EXTRA_VOICES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'voices')
+extra_voice_dirs=[MY_EXTRA_VOICES_DIR]
+
+
+vocalize_txt(init_tts(), txt_2b_voiced, "venti", "fast", os.path.join(os.environ['USERPROFILE'],f"downloads/venti_ai.wav"), extra_voice_dirs)
